1337ed6f3467e34888d5dd227b9778045181661b
allenjuan <103193558+yulunjuan@users.noreply.github.com>
2025-11-03 15:54:31 -0800

    Initial commit

---
+++ src/main/java/com/profolio/stock_profolio_tracker/StockProfolioTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.profolio.stock_profolio_tracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StockProfolioTrackerApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StockProfolioTrackerApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/profolio/stock_profolio_tracker/StockProfolioTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.profolio.stock_profolio_tracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StockProfolioTrackerApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
